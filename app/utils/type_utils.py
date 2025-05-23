e660d7bfcc692bd369fb8e5fde93983bd88f5c55
Shaya94 <yginzburg94@gmail.com>
2025-05-23 22:49:44 +0300

    Remove all contents before Copilot code

--- app/utils/type_utils.py
+++
@@ -1,2 +0,0 @@
-def create_type_query(type):
-    return {"$regex": f"^{type}$", "$options": "i"}
--- client/src/components/IncomeDetailPage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ItemDetailPage } from "./ItemDetailPage";
-
-const IncomeDetailPage = () => {
-    return <ItemDetailPage type="income" />;
-};
-
-export default IncomeDetailPage;
