3af83638d44a39ab34bd27e2c365dc5784f28b38
Shaya94 <yginzburg94@gmail.com>
2025-07-03 23:29:49 +0300

    Update mongodb connection func

--- app.js
+++ app.js
@@ -13,10 +13,7 @@ const getCurrentMonthYear = require("./utils/dates/getCurrentMonthYear");
 
 mongoose.set("strictQuery", true);
 mongoose
-    .connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("MongoDB connection error:", err));
 
